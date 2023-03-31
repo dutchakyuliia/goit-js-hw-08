3bf0d418ac52a4d2a65773df5913f83e5cca4fad
Dutchak Yuliia <dutchakyuliia@gmail.com>
2023-03-31 17:52:02 +0200

    make gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector(".gallery");
+galleryList.style.listStyle = "none";
+const markup = galleryItems
+  .map(
+    (item) =>
+      `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img class="gallery__image" src="${item.preview}" alt="${item.description}" title="${item.description}" />
+            </a>
+        </li>`
+  )
+  .join("");
+
+console.log(markup);
+galleryList.innerHTML = markup;
+
+let lightbox = new SimpleLightbox(".gallery a", {});
+
